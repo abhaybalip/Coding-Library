7b3f19901bca6383b9cf61bc0a0fef8b2e0d71b4
Abhay Balip <abhay.balip@gmail.com>
2023-02-02 22:49:34 +0530

    Add files via upload

--- SQL Language/SQL 3.0/Procedure.sql
+++ SQL Language/SQL 3.0/Procedure.sql
@@ -5,18 +5,20 @@ CREATE TABLE Test(
     id int,
     user char(10)
 );
-INSERT INTO Test VALUES(1,'u1');
-INSERT INTO Test VALUES(2,'u2');
-INSERT INTO Test VALUES(3,'u3');
-
 -- create procedure
-
--- call procedure
-
--- parametrised procedure
-CREATE procedure Insert_user(in pid int,in puser char(10))
+CREATE procedure InsertData(In pid int,In puser char(10))
 begin
-insert into Test VALUES(pid,puser);
+insert into Test values(pid,puser);
 end;
 
-call Insert_user(10,'u10');
+-- call procedure
+call InsertData(10,'u10');
+call InsertData(20,NULL);
+
+CREATE procedure DisplayData(In pid int)
+begin
+select id,user from Test where id = pid;
+end;
+call DisplayData(10);
+
+drop DATABASE Test_db;
