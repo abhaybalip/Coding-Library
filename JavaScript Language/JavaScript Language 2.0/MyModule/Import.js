d7b9e4f5e56d07ebca5190045aa6cd1e38de174d
Abhay Balip <abhay.balip@gmail.com>
2023-02-03 20:58:09 +0530

    Add files via upload

--- JavaScript Language/JavaScript Language 2.0/MyModule/Import.js
+++
@@ -1,8 +0,0 @@
-    // java script - import module
-    // import from filr
-    import { println } from './Module.js';
-    import {name,id,obj,arr} from './Module.js';
-    println('Hi')
-    println(name)
-    println(id)
-    println(obj)
