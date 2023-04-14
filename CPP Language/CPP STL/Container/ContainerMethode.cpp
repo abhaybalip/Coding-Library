4e81e446f4b7cbcdca9c3130dbf4ffab27dce636
Abhay Balip <abhay.balip@gmail.com>
2023-04-14 19:08:54 +0530

    Add files via upload

---
+++ CPP Language/CPP STL/Container/ContainerMethode.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+    std::vector<int> v{0,1,2,3,4},v2;
+int main(){
+    // common methodes of container
+
+    // Data Manipulation
+    v.push_back(10);
+    v.pop_back();
+    v2.assign(v.begin(),v.end());
+    v.insert(v.begin(),1);
+    v.clear();
+    v.erase(v.begin());
+    v2.swap(v);
+    v.resize(5);
+    
+    // Properties
+    int size = v.size();
+    int max_size = v.max_size();
+    int capacity = v.capacity();
+    bool b = v.empty();
+
+    // return iterator
+    v.begin();
+    v.end();
+    
+    // return data
+    v.front();
+    v.back();
+    v.at(0);
+
+
+    return 0;
+}
---
+++ CPP Language/CPP STL/Container/Vector.cpp
@@ -0,0 +1,11 @@
+// cpp - STL - Vector
+#include<iostream>
+#include<vector>
+int main(){
+    // Vector declaration
+    std::vector<int> v1;
+    std::vector<char> v2(10);
+    std::vector<std::string> v3{"Hi","Hello","Welcome"};
+
+    return 0;
+}
