a6d5cd83788f0eac124d670bbd4c474d530d07c5
Abhay Balip <abhay.balip@gmail.com>
2023-06-06 11:49:58 +0530

    Add files via upload

--- C/C Adv/FileManagement.c
+++ C/C Adv/FileManagement.c
@@ -1,42 +1,39 @@
-// c - File Management
+// File Management
 #include<stdio.h>
 #include<stdlib.h>
-char ch[10];
 int main(){
-    // file opening modes : r - read , w - write , a - creat and write
-    // r+ - read and write both
-    // opening file
-    FILE *f1 = fopen("C:\\Users\\abhay\\OneDrive\\Documents\\GitHub\\test.txt","w+");
-    FILE *f2 = fopen("C:\\Users\\abhay\\OneDrive\\Documents\\GitHub\\test.txt","r+");
-    int n=10,n2,n3;
-    char c='A',c2,c3;
-    // write to file
-    fprintf(f1,"%d %c",n,c);
 
-    // close file
-    fclose(f1);
+    // Opening File : 
+    FILE* file = fopen("TestFile.txt", "a+");
+    // Closing File : 
+    fclose(file);
 
-    // read from file
-    fscanf(f2,"%d %c",&n2,&c2);
-    fclose(f2);
-    printf("Data Read from file : %d %c \n",n2,c2);
+    file = fopen("TestFile.txt", "w+");
+    int array[] = {0, 1, 2, 3, 4};
+    char ch = 'A';
 
+    size_t elements = fwrite(array,sizeof(int),sizeof(array)/sizeof(int),file);
+    // elements = fwrite(&ch, sizeof(char), 1, file);
 
-    // string methode for file
-    f1 = fopen("C:\\Users\\abhay\\OneDrive\\Documents\\GitHub\\test2.txt","w+");
-    fputs("Hello World",f1);
-    fclose(f1);
+    fclose(file);
 
-    f2 = fopen("C:\\Users\\abhay\\OneDrive\\Documents\\GitHub\\test2.txt","r+");
-    fgets(ch,12,f2);
-    int index=0;
-    while(ch[index]!='\0'){
-        printf("%c",ch[index]);
-        index++;
+    file = fopen("TestFile.txt", "r");
+    int* arr = malloc(sizeof(int)*(sizeof(array)/sizeof(int)));
+    elements = fread(arr,sizeof(int),sizeof(array)/sizeof(int),file);
+    fclose(file);
+
+    for (int i=0;i<sizeof(array)/sizeof(int);i++){
+        printf("%d ", arr[i]);
     }
 
-    // write array to file
-    char str[] = "Hello User";
-    fwrite(str,1,sizeof(str),f2);
+
+    // File index pointer
+    // file pointer , offset , origin
+    // origin : seek_set , seek_cur , seek_end
+    fseek(file,0,SEEK_SET);
+
+    free(arr);
+    fclose(file);
+
     return 0;
 }
---
+++ C/C Adv/FileStream.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main() {
+    // opening file
+    FILE *f1 = fopen("test.txt","w+");
+    FILE *f2 = fopen("test.txt","r+");
+
+    int n = 10, n2, n3;
+    char c = 'A', c2, c3;\
+
+    // write to file
+    fprintf(f1, "%d %c", n, c);
+
+    // Move the file pointer to the beginning of the file
+    fseek(f1, 0, SEEK_SET);
+
+    // read from file
+    fscanf(f1, "%d %c", &n2, &c2);
+
+    // print the data read from the file
+    printf("Data Read from file: %d %c\n", n2, c2);
+
+    // close file
+    fclose(f1);
+
+    // string method for file
+    f1 = fopen("test2.txt", "w+");
+    fputs("Hello World", f1);
+    fclose(f1);
+
+    f2 = fopen("test2.txt", "r+");
+    char str[12]; // array to store the string
+
+    // read string from file
+    fgets(str, sizeof(str), f2);
+    printf("String Read from file: %s\n", str);
+
+    // write array to file
+    char str2[] = "Hello User";
+    fwrite(str2, 1, sizeof(str2), f2);
+
+    // Move the file pointer to the beginning of the file
+    fseek(f2, 0, SEEK_SET);
+
+    // read array from file
+    char str3[sizeof(str2)];
+    fread(str3, 1, sizeof(str2), f2);
+    printf("Array Read from file: %s\n", str3);
+
+    fclose(f2);
+
+    return 0;
+}
