2f24cfba7f96065a2abed6383a7261a38d7f7137
Abhay Balip <abhay.balip@gmail.com>
2023-06-15 13:14:35 +0530

    Add files via upload

---
+++ JavaScript/JS Adv/bind.js
@@ -0,0 +1,4 @@
+// (method) Function.bind(this: Function, thisArg: any, ...argArray: any[]): any
+// For a given function, creates a bound function that has the same body as the original function
+// The this object of the bound function is associated with the specified object
+// and has the specified initial parameters.
